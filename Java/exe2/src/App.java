984b5edfbfb06ba5ba6e4f97e69aaab0f9ed1b96
Samuel Carvalho <144500808+carvalh0-samuel@users.noreply.github.com>
2026-03-06 10:50:07 -0300

    Refactor main method to count down and use while loop

--- Java/exe2/src/App.java
+++ Java/exe2/src/App.java
@@ -1,7 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        for (int i = 0; i < 10; i = i + 1){
+        for (int i = 10; i > 0; i--){
             System.out.println("Contador: "+i);
+        }
+        for (int i = 10; i < 0; i++){
+            System.out.println("Contador 2: "+i);
+        }
+        int idade = 0;
+        while (idade<20) {
+            System.out.println("Idade: "+idade);
+            idade++;
         }
        
     }
